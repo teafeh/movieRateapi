47ac943a2a569a957ff262cd8354b038b6b52bfe
teafeh <boluwateafeh24@gmail.com>
2025-10-17 16:45:38 +0100

    init commit

---
+++ server.js
@@ -0,0 +1,24 @@
+require("dotenv").config()
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express()
+
+const movieRoute = require("./router/movie.route")
+app.use(express.json())
+app.use("/", movieRoute)
+const PORT = 3100 
+
+const start = async () => {
+    try {
+        await mongoose.connect(process.env.EVN_URL_MONDB);
+        app.listen(PORT, () => {
+          console.log(`server is live on port http://localhost:${PORT}`);
+          console.log(`mongodb conneted`);
+        });
+    } catch (error) {
+        console.log(error, "internal server error");
+        
+    }
+}
+
+start()
